5df94e140995dde6a5491db85f5cd6d4d833afb9
zachtyson <80659983+zachtyson@users.noreply.github.com>
2021-08-22 01:15:53 -0400

    Added game over screen
    Game basically complete

--- src/game.java
+++ src/game.java
@@ -6,8 +6,7 @@ import java.util.TimerTask;
 import java.util.Timer;
 
 //SNAKE GAME
-//CURRENTLY THE ONLY WORKING FUNCTION IS ADDING BODY PARTS
-
+//ALMOST COMPLETELY FUNCTIONAL
 
 public class game {
 
@@ -17,18 +16,18 @@ public class game {
     Action leftMovement;
     Action rightMovement;
 
+    JTextArea endGame;
     final int MAP_SIZE = 400;
     final int NUM_TILES = 20;
     Timer timer;
     int movementDirection = 0;
-
+    int score = 0;
     JLabel foodTile;
     //I decided to do AbstractAction instead of ActionListener for movements.
     //I don't remember why I chose that honestly
 
     ArrayList<snakeBody> body = new ArrayList<>(); //Each square on the snake's body is going to be a label, which is part of an array of each square
 
-    int parts = 0; //Number of body parts, used for the addBodyPart function
     game() {
 
         frame = new JFrame("Snake game");
@@ -127,8 +126,7 @@ public class game {
         }
 
         body.add(new snakeBody(body.get(body.size()-1).getX() + x , body.get(body.size()-1).getY() + y, MAP_SIZE/NUM_TILES, MAP_SIZE/NUM_TILES));
-        parts++;
-        frame.add(body.get(parts));
+        frame.add(body.get(body.size() -1));
     }
     public boolean checkIfSameTile(snakeBody labelCheck){
         if(labelCheck.getX() == foodTile.getX() && labelCheck.getY() == foodTile.getY()) { //Method checks to see if the food is in the same location as the snake head, if it is, then move the food to random spot
@@ -169,32 +167,59 @@ public class game {
         }
         checkIfDead(body.get(0));
     }
-    public void moveFood() {
+    public void moveFood() { //Literally just moves the red panel to a random spot on the frame
         int randX = (int)(Math.random()*MAP_SIZE/NUM_TILES)*MAP_SIZE/NUM_TILES;
         int randY = (int)(Math.random()*MAP_SIZE/NUM_TILES)*MAP_SIZE/NUM_TILES;
         foodTile.setLocation(randX,randY);
+        score++;
+
     }
-    public boolean checkIfDead(snakeBody head) {
+    public void checkIfDead(snakeBody head) { //Checks to see if the player has lost, either by eating themself or touching the outer boundaries of the map
         if(body.size() > 1) {
             for (int i = 1; i < body.size(); i++) {
                 if (head.getX() == body.get(i).getX() && head.getY() == body.get(i).getY()) {
                     timer.cancel();
-                    return true;
+                    lostGame();
+                    return;
                 }
             }
         }
         if(head.getX() < 0 || head.getX() >= MAP_SIZE || head.getY() < 0 || head.getY() >= MAP_SIZE) {
             timer.cancel();
-            return true;
+            lostGame();
         }
-        return false;
     }
     public boolean checkIfDead(int x, int y) {
         if(x < 0 ||x >= MAP_SIZE || y < 0 || y >= MAP_SIZE) {
             timer.cancel();
+            lostGame();
             return true;
         }
         return false;
+    }
+    public void lostGame(){  //You just lost The Game
+        Timer timerEnd = new Timer();
+
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                for (snakeBody snakeBody : body) {
+                    snakeBody.setVisible(false);
+                }
+                foodTile.setVisible(false);
+                endGame = new JTextArea();
+                endGame.setFont(new Font("Trebuchet MS", Font.BOLD, MAP_SIZE/10));
+                endGame.setForeground(Color.green);
+                endGame.setSize(new Dimension(MAP_SIZE, MAP_SIZE));
+                endGame.setBackground(Color.BLACK);
+                endGame.setText("GAME OVER \n" + "SCORE: " + score);
+                frame.repaint();
+                frame.add(endGame);
+            } //Removes snake and food, just puts a game over screen
+        };
+
+        timerEnd.schedule(task, 2000); //Does this after 2 seconds
     }
 
 }
