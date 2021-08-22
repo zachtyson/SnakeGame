e6e6ac8215a7ba3a059843008904a28ba5669c18
zachtyson <80659983+zachtyson@users.noreply.github.com>
2021-08-21 23:44:18 -0400

    Added comments, removed a few unused variables

--- src/game.java
+++ src/game.java
@@ -3,6 +3,9 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
+//SNAKE GAME
+//CURRENTLY THE ONLY WORKING FUNCTION IS ADDING BODY PARTS
+
 
 public class game {
 
@@ -27,12 +30,11 @@ public class game {
         frame.setLocationRelativeTo(null);
 
 
-        body.add(new snakeBody(0,0));
+        body.add(new snakeBody(0,0)); //Creates the "head" of the snake
         upMovement = new UpMovement();
         downMovement = new DownMovement();
         leftMovement = new LeftMovement();
         rightMovement = new RightMovement();
-
         body.get(0).getInputMap().put(KeyStroke.getKeyStroke("UP"), "upMovement");
         body.get(0).getActionMap().put("upMovement", upMovement);
         body.get(0).getInputMap().put(KeyStroke.getKeyStroke("DOWN"), "downMovement");
@@ -42,10 +44,14 @@ public class game {
         body.get(0).getInputMap().put(KeyStroke.getKeyStroke("RIGHT"), "rightMovement");
         body.get(0).getActionMap().put("rightMovement", rightMovement);
 
+        //Keybindings for the movement, up down left and right
+
         frame.add(body.get(0));
         frame.setVisible(true);
     }
 
+    //All of these are just keybindings for movement, and are identical otherwise
+    //Optimization very possible, but I wanna get this to work
     public class UpMovement extends AbstractAction {
         @Override
         public void actionPerformed(ActionEvent e) {
@@ -99,18 +105,20 @@ public class game {
             }
         }
     }
-    public void addBodyPart(int a) {
-        int x = 0;
-        int y = 0;
+    public void addBodyPart(int a) { //Adds additional squares behind the snake, adding each one to the body ArrayList
+        int x = 0; //X coordinate
+        int y = 0; //Y coordinate
+                    // both defaulted to zero
+        //1 = up, 2 = down, 3 = left, 4 = right for int a
         if(a < 3) {
-            y = 20;
-            if(a == 1) {
+            y = 20;  //if a = 1 or 2 then it means vertical
+            if(a == 1) { //1 means upwards movement
                 y = -y;
             }
         }
         if(a > 2) {
-            x = -20;
-            if(a == 3) {
+            x = -20;//if a = 3 or 4 then it means horizontal movement
+            if(a == 3) { //3 means left movement
                 x = -x;
             }
         }
@@ -120,7 +128,7 @@ public class game {
         frame.add(body.get(parts));
     }
     public boolean checkIfSameTile(snakeBody labelCheck){
-        return labelCheck.getX() == 200 && labelCheck.getY() == 200;
+        return labelCheck.getX() == 200 && labelCheck.getY() == 200; //This is just for testing to see if the body part movements work, at 200,200 it counts as an "apple" or food source like the original game
     }
     public void moveSnake(int direction, int distance) {
         //1 = up, 2 = down, 3 = left, 4 = right
@@ -138,10 +146,8 @@ public class game {
                 x = -x;
             }
         }
+        //this for loop moves each body part relative to where the part ahead of it (in the ArrayList) was previously, and goes all the way up to the 0th index
         if(body.size() > 1) {
-            int x1;
-            int y1;
-
             for (int i = body.size() -1; i > 0; i--) {
                 int y2 = body.get(i-1).getY();
                 int x2 = body.get(i-1).getX();
@@ -151,16 +157,18 @@ public class game {
             body.get(0).setCoordinates(x,y);
     }
 }
-class snakeBody extends JLabel{
+
+
+class snakeBody extends JLabel{ //Again, each snake part is a JLabel, with coordinates attached to it under this class
     snakeBody(int x, int y) {
         this.setBackground(Color.GREEN);
         this.setBounds(x, y, 20, 20);
         this.setOpaque(true);
     }
-    public void setCoordinates(int x, int y) {
+    public void setCoordinates(int x, int y) { //this is just for the head of the snake, to move it relative to the direction pressed
         this.setLocation(this.getX() + x, this.getY() + y);
     }
-    public void moveUp(int x, int y) {
+    public void moveUp(int x, int y) { //This moves the rest of the snake body parts, honestly a little unnecessary since you can just directly do it, but I did this for sake of readability
         this.setLocation(x,y);
     }
 }
